bb67f6de26e117ecf02bf742527f67e99eb37f75
Yurii Shutkin <yurii.shutkin@gmail.com>
2024-02-03 20:11:29 +0100

    fix CI

--- techlibs/common/cellhelp.py
+++ techlibs/common/cellhelp.py
@@ -27,27 +27,29 @@ def print_current_cell(f):
         ),
         file=f,
     )
+if (len(sys.argv) > 2):
+	f = open(sys.argv[2], "w")
+else:
+	f = stdout
 
-
-with open(sys.argv[2], "w") as f:
-    for line in fileinput.input(sys.argv[1]):
-        if line.startswith("//-"):
-            current_help_msg.append(line[4:] if len(line) > 4 else "\n")
-        if line.startswith("module "):
-            current_module_name = line.split()[1].strip("\\")
-            current_module_signature = " ".join(
-                line.replace("\\", "").replace(";", "").split()[1:]
-            )
-            current_module_code = []
-        elif not line.startswith("endmodule"):
-            line = "    " + line
-        current_module_code.append(line.replace("\t", "    "))
-        if line.startswith("endmodule"):
-            if len(current_help_msg) == 0:
-                current_help_msg.append("\n")
-                current_help_msg.append("    %s\n" % current_module_signature)
-                current_help_msg.append("\n")
-                current_help_msg.append("No help message for this cell type found.\n")
-                current_help_msg.append("\n")
-            print_current_cell(f)
-            current_help_msg = []
+for line in fileinput.input(sys.argv[1]):
+    if line.startswith("//-"):
+        current_help_msg.append(line[4:] if len(line) > 4 else "\n")
+    if line.startswith("module "):
+        current_module_name = line.split()[1].strip("\\")
+        current_module_signature = " ".join(
+            line.replace("\\", "").replace(";", "").split()[1:]
+        )
+        current_module_code = []
+    elif not line.startswith("endmodule"):
+        line = "    " + line
+    current_module_code.append(line.replace("\t", "    "))
+    if line.startswith("endmodule"):
+        if len(current_help_msg) == 0:
+            current_help_msg.append("\n")
+            current_help_msg.append("    %s\n" % current_module_signature)
+            current_help_msg.append("\n")
+            current_help_msg.append("No help message for this cell type found.\n")
+            current_help_msg.append("\n")
+        print_current_cell(f)
+        current_help_msg = []
